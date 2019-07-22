3fcb094c14e096e8c53a11e359ac8c2a18ba67a7
Muhammad Rizki Agus Salim <31557933+rizkiaguss@users.noreply.github.com>
2019-07-22 21:50:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,89 +1,82 @@
-<html>
- <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
- </head>
- <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
- <?php
-    $host = "rizkiagussappserver.database.windows.net,1433";
-    $user = "rizkiaguss";
-    $pass = "qwerTY22";
-    $db = "Registration";
-
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-
-    if (isset($_POST['submit'])) {
-        try {
+<!doctype html>
+<html lang="en">
+ 
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+   <title>Submission Azure</title>
+</head>
+ 
+<body>
+   <div class="container mt-3">
+      <h1 class="center">Register Here</h1>
+ 
+      <form action="index.php" method="post">
+         <div class="form-group">
+            <label for="name">Your Name</label>
+            <input type="text" name="name" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="email">Your Email</label>
+            <input type="email" name="email" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="job">Your Job</label>
+            <input type="text" name="job" class="form-control">
+         </div>
+         <input type="submit" name="simpan" class="btn btn-primary btn-md">
+      </form>
+      <?php
+      $host = "khaidirwebserver.database.windows.net,1433";
+      $user = "khaidir";
+      $pass = "naruto123#";
+      $db = "khaidirwebappserver";
+      try {
+         $con = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pass);
+         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (Exception $e) {
+         echo "Failed : " . $e;
+      }
+      if (isset($_POST['simpan'])) {
+         try {
             $name = $_POST['name'];
             $email = $_POST['email'];
             $job = $_POST['job'];
-            $date = date("Y-m-d");
-            // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (?,?,?,?)";
-            $stmt = $conn->prepare($sql_insert);
+            $query = "INSERT INTO Woker (name, email, job) VALUES (?,?,?)";
+            $stmt = $conn->prepare($query);
             $stmt->bindValue(1, $name);
             $stmt->bindValue(2, $email);
             $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
             $stmt->execute();
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
-            }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-    }
- ?>
- </body>
- </html>
+         } catch (Exception $e) {
+            echo "Failed" . $e;
+         }
+         echo "<h3>Your're registered!</h3>";
+      }
+      ?>
+      <table class="table table-responsive mt-3">
+         <thead>
+            <tr>
+               <th>Name</th>
+               <th>Email</th>
+               <th>Job</th>
+            </tr>
+         </thead>
+         <tbody>
+ 
+         </tbody>
+      </table>
+   </div>
+   <!-- Optional JavaScript -->
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+ 
+</html>
